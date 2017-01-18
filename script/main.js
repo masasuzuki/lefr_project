d8fa13acc1b77d12188777a28e3aa84765f14719
masataka_suzuki <masataka_suzuki@worksjapan.co.jp>
2017-01-19 00:26:58 +0900

    デザイン変更前

---
+++ script/main.js
@@ -0,0 +1,18 @@
+$(function(){
+	//button1の制御
+	$('.button1 a').on('mouseover',function(){
+		$(this).stop().animate({
+			backgroundColor:'#00CCFF',
+			borderColor:'white',
+			color:'white'
+		},500
+		);
+	}).on('mouseout',function(){
+		$(this).stop().animate({
+			backgroundColor:'transparent',
+			borderColor:'#01044E',
+			color:'#01044E'
+		},500
+		);
+	});
+});
