e31a01e604f1d0c1652cd0636f28d4cf4dcdec87
masasuzuki <match.suzuki@gmail.com>
2017-02-19 18:17:48 +0900

    登録の流れ：デザイン変更

--- script/main.js
+++ script/main.js
@@ -2,6 +2,22 @@ $(function(){
 	//button1の制御
 	$('.button1 a').on('mouseover',function(){
 		$(this).stop().animate({
+			backgroundColor:'#3CA89F',
+			borderColor:'#FFF',
+			color:'#FFF'
+		},500
+		);
+	}).on('mouseout',function(){
+		$(this).stop().animate({
+			backgroundColor:'transparent',
+			borderColor:'transparent',
+			color:'#01044E'
+		},500
+		);
+	});
+	//button2の制御
+	$('.button2 a').on('mouseover',function(){
+		$(this).stop().animate({
 			backgroundColor:'#00CCFF',
 			borderColor:'#FFF',
 			color:'#FFF'
@@ -10,8 +26,8 @@ $(function(){
 	}).on('mouseout',function(){
 		$(this).stop().animate({
 			backgroundColor:'transparent',
-			borderColor:'#01044E',
-			color:'#01044E'
+			borderColor:'#FFF',
+			color:'#FFF'
 		},500
 		);
 	});
