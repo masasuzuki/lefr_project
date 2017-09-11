fa002261df58833647d234da738f88f85153b4b8
masasuzuki <match.suzuki@gmail.com>
2017-09-11 20:13:20 +0900

    コンサルコメント画像に変更

--- script/main.js
+++ script/main.js
@@ -2,15 +2,15 @@ $(function(){
 	//button1の制御
 	$('.button1 a').on('mouseover',function(){
 		$(this).stop().animate({
-			backgroundColor:'#3CA89F',
+			backgroundColor:'#00CCFF',
 			borderColor:'#FFF',
 			color:'#FFF'
 		},500
 		);
 	}).on('mouseout',function(){
 		$(this).stop().animate({
-			backgroundColor:'transparent',
-			borderColor:'transparent',
+			backgroundColor:'#FFF',
+			borderColor:'#01044E',
 			color:'#01044E'
 		},500
 		);
@@ -28,6 +28,23 @@ $(function(){
 			backgroundColor:'transparent',
 			borderColor:'#FFF',
 			color:'#FFF'
+		},500
+		);
+	});
+
+	//button3の制御
+	$('.button3 a').on('mouseover',function(){
+		$(this).stop().animate({
+			backgroundColor:'#00CCFF',
+			borderColor:'#FFF',
+			color:'#FFF'
+		},500
+		);
+	}).on('mouseout',function(){
+		$(this).stop().animate({
+			backgroundColor:'transparent',
+			borderColor:'#01044E',
+			color:'#01044E'
 		},500
 		);
 	});
--- submit.php
+++ submit.php
@@ -3,7 +3,11 @@ header("Content-Type: text/html;charset=utf-8");
 mb_language('ja');
 mb_internal_encoding( "utf-8" );
 
-if(mb_send_mail($_POST[mail], "てすと", "これはテストメールです\n".$_POST[name]."さん登録ありがとうございます。\n登録頂いた内容は以下の通りです。\n-------\nメールアドレス：".$_POST[mail]."\n生まれた年：".$_POST[birthyear]."\n居住地：".$_POST[pref]."\n経験職種：".$_POST[experienceJob]."\n希望職種：".$_POST[desireJob]."\n備考：\n".$_POST[free_coment]."\n-------\n担当者より連絡致します。", "From: match.suzuki@gmail.com", '-f match.suzuki@gmail.com')){
+if (isset($_POST['experienceJob']) && is_array($_POST['experienceJob'])) {
+    $experienceJob = implode("、", $_POST["experienceJob"]);
+}
+
+if(mb_send_mail("match.suzuki@gmail.com", "※テスト※【要対応】個人事業主様よりご応募がありました", "これはテストメールです\n\n担当者各位\n\n".$_POST[name]."（".$_POST[name_kana]."）様よりご応募がありました。\nご登録いただいた内容は以下の通りです。\n====================================\nメールアドレス：".$_POST[mail]."\n生まれた年：".$_POST[birthyear]."\n居住地：".$_POST[pref].$_POST[address]."\n経験職種：".$experienceJob."\n希望職種：".$_POST[desireJob]."\nフリーメッセージ：\n".$_POST[free_coment]."\n====================================\nご担当者より個人事業主様に連絡をお願いします。\n\n※このメールに心当たりがない場合は大変お手数ですがメールを削除してください。\n\n---\nLIVE ESTフリーランスシステム\n---", "From: match.suzuki@gmail.com", '-f match.suzuki@gmail.com')){
 	echo "メールが送信されました";
 }else{
 	echo "メールの送信に失敗しました";
