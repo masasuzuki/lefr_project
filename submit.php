42c4d411c361b754ace4b78594be9c7dff048c66
masasuzuki <match.suzuki@gmail.com>
2017-09-12 17:11:33 +0900

    フォーム完了時JS、PHP修正。登録完了画面追加

--- submit.php
+++ submit.php
@@ -7,10 +7,15 @@ if (isset($_POST['experienceJob']) && is_array($_POST['experienceJob'])) {
     $experienceJob = implode("、", $_POST["experienceJob"]);
 }
 
-if(mb_send_mail("match.suzuki@gmail.com", "※テスト※【要対応】個人事業主様よりご応募がありました", "これはテストメールです\n\n担当者各位\n\n".$_POST[name]."（".$_POST[name_kana]."）様よりご応募がありました。\nご登録いただいた内容は以下の通りです。\n====================================\nメールアドレス：".$_POST[mail]."\n携帯番号：".$_POST[tel_mobile]."\n生まれた年：".$_POST[birthyear]."\n居住地：".$_POST[pref].$_POST[address]."\n経験職種：".$experienceJob."\n希望職種：".$_POST[desireJob]."\nフリーメッセージ：\n".$_POST[free_coment]."\n====================================\nご担当者より個人事業主様に連絡をお願いします。\n\n※このメールに心当たりがない場合は大変お手数ですがメールを削除してください。\n\n---\nLIVE ESTフリーランスシステム\n---", "From: match.suzuki@gmail.com", '-f match.suzuki@gmail.com')){
-	echo "メールが送信されました";
+if(mb_send_mail("match.suzuki@gmail.com", "※テスト※【要対応】個人事業主様よりご応募がありました", "これはテストメールです\n\n担当者各位\n\n".$_POST[name]."（".$_POST[name_kana]."）様よりご応募がありました。\nご登録いただいた内容は以下の通りです。\n====================================\nメールアドレス：".$_POST[mail]."\n携帯番号　　　：".$_POST[tel_mobile]."\n生まれた年　　：".$_POST[birthyear]."\n最寄り駅　　　：".$_POST[pref].$_POST[address]."\n経験職種　　　：".$experienceJob."\n希望職種　　　：".$_POST[desireJob]."\nフリーメッセージ：\n".$_POST[free_coment]."\n====================================\nご担当者より個人事業主様に連絡をお願いします。\n\n※このメールに心当たりがない場合は大変お手数ですがメールを削除してください。\n\n---\nLIVE ESTフリーランスシステム\n---", "From: match.suzuki@gmail.com", '-f match.suzuki@gmail.com')){
+	/*echo "メールが送信されました";*/
+	$url = 'form_finish.html';
+	header("Location: {$url}");
+	exit();
 }else{
-	echo "メールの送信に失敗しました";
+	$url = 'form.html';
+	header("Location: {$url}");
+	exit();
 }
 
 ?>
